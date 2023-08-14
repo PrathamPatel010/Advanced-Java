bcd9377c273e77dda5524f0945e84ddc55837715
Pratham <ppratham812@gmail.com>
2023-08-14 21:25:14 +0530

    Practical-2 code quality improved and .gitignore file added

--- ServletPrograms/src/java/Practical2/DisplayData.java
+++ ServletPrograms/src/java/Practical2/DisplayData.java
@@ -5,7 +5,11 @@ import javax.servlet.http.*;
 
 public class DisplayData extends HttpServlet{
     public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException,ServletException{
+        HttpSession session = req.getSession();
+        session.setMaxInactiveInterval(10);
         PrintWriter out = res.getWriter();
+        out.println("Session ID: " + session.getId());
+        out.println("Session Creation time: " + session.getCreationTime());
         String name = req.getParameter("name");
         String age = req.getParameter("age");
         out.println("Name is: " +name);
